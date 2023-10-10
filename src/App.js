f904385353da9bb0b7d5750278b93c91a6307c5b
TipTopClone <sujanmahat2468@gmail.com>
2023-10-10 11:16:24 +1100

    first branch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 import Hero from './Components/Hero';
 import Info from './Components/Info';
 import Navbar from './Components/Navbar';
 import Skills from './Components/Skills';
+import Works from './Components/Work';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='wrapper'>
-      <Navbar />
-      <Hero />
-      <Info />
-      <Skills />
-    </div>
+    <>
+      <input type='checkbox' id='darkMode' />
+      <div className='wrapper'>
+        <label htmlFor='darkMode' className='dark-mode-toggle'>
+          <i className='fa-solid fa-circle-half-stroke'></i>
+        </label>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Hero />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/projects' element={<Works />} />
+          <Route path='/about-me' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+
+        {/* <Hero />
+        <Info />
+        <Skills />
+        <Works />
+        <About />
+        <Contact /> */}
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Work.js
@@ -0,0 +1,72 @@
+import projectImg from '../Assets/Screenshot 2023-08-15 at 3.03.17 pm.png';
+const Works = () => {
+  return (
+    <section id='projects' className='container projects'>
+      <h2 className='title'>My Recent Works</h2>
+
+      <div className='grid projects-container'>
+        <div className='project-card'>
+          <div className='top'>
+            <img src={projectImg} alt='project-img' />
+          </div>
+          <div className='bottom container'>
+            <h3>Personal portfolio website</h3>
+            <p>October 2023</p>
+            <a href=''>
+              <i className='fa-brands fa-github'></i>
+            </a>
+            <a href=''>
+              <i className='fa-brands fa-chrome'></i>
+            </a>
+          </div>
+        </div>
+        <div className='project-card'>
+          <div className='top'>
+            <img src={projectImg} alt='project-img' />
+          </div>
+          <div className='bottom container'>
+            <h3>E-Commerce website</h3>
+            <p>October 2023</p>
+            <a href=''>
+              <i className='fa-brands fa-github'></i>
+            </a>
+            <a href=''>
+              <i className='fa-brands fa-chrome'></i>
+            </a>
+          </div>
+        </div>
+        <div className='project-card'>
+          <div className='top'>
+            <img src={projectImg} alt='project-img' />
+          </div>
+          <div className='bottom container'>
+            <h3>CMS System</h3>
+            <p>October 2023</p>
+            <a href=''>
+              <i className='fa-brands fa-github'></i>
+            </a>
+            <a href=''>
+              <i className='fa-brands fa-chrome'></i>
+            </a>
+          </div>
+        </div>
+        <div className='project-card'>
+          <div className='top'>
+            <img src={projectImg} alt='project-img' />
+          </div>
+          <div className='bottom container'>
+            <h3>SaaS</h3>
+            <p>October 2023</p>
+            <a href=''>
+              <i className='fa-brands fa-github'></i>
+            </a>
+            <a href=''>
+              <i className='fa-brands fa-chrome'></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Works;
